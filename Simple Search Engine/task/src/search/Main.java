2c7fa6df077bd16d300fa39a6e0e81ab374c2eed
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2020-08-06 20:21:58 -0700

    Stage 1: Program accepts standard input of a sequence of words in the first line, and a word to search for in the second line. Output is the index of the searched word (beginning at index 1).

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,21 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split("\\s");
+        String target = scanner.next();
+
+        // if text contains target, print index
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+        // else print "Not Found"
+        System.out.println("Not Found");
     }
 }
