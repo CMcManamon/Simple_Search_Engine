3832735c0a189a172eec17146b2c0c4fbf0239b0
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2020-08-07 20:10:14 -0700

    Stage 3: Added a menu that allows the user to choose between options. Can search for an entry. Can print all entries. Can exit program.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -16,7 +16,7 @@ public class Main {
     }
 }
 
-enum QueryState {NUMBER_OF_ANIMALS, ANIMAL_ENTRY, NUMBER_OF_QUERIES, DATA_TO_FIND}
+enum QueryState {NUMBER_OF_ANIMALS, ANIMAL_ENTRY, MENU_OPTIONS, DATA_TO_FIND}
 
 class SearchEngine {
     QueryState queryState = QueryState.NUMBER_OF_ANIMALS;
@@ -25,48 +25,98 @@ class SearchEngine {
     int inputCount = 0;
 
     public SearchEngine() {
-        System.out.println("Enter the number of animals:");
+        promptNumAnimals();
     }
 
     public void processInput(String input) {
         switch (queryState) {
             case NUMBER_OF_ANIMALS:
-                inputCount = Integer.parseInt(input); // ToDo: exception check
-                queryState = QueryState.ANIMAL_ENTRY;
-                System.out.println("Enter all animals:");
+                inputCount = stringToNumAnimals(input);
+                promptEnterAnimals();
                 break;
             case ANIMAL_ENTRY:
-                if (inputCount > 0) {
-                    animals.add(input);
-                    inputCount--;
-                }
-                if (inputCount <= 0) {
-                    inputCount = 0;
-                    queryState = QueryState.NUMBER_OF_QUERIES;
-                    System.out.println("\nEnter the number of search queries:");
-                }
+                enterAnimal(input);
                 break;
-            case NUMBER_OF_QUERIES:
-                inputCount = Integer.parseInt(input); // ToDo: exception check
-                queryState = QueryState.DATA_TO_FIND;
-                System.out.println("\nEnter data to search for animals:");
+            case MENU_OPTIONS:
+                processMenuOption(input);
                 break;
             case DATA_TO_FIND:
-                if (inputCount > 0) {
-                    searchForAnimal(input);
-                    inputCount--;
-                }
-                if (inputCount <= 0) {
-                    acceptingInput = false;
-                    inputCount = 0;
-                    queryState = QueryState.NUMBER_OF_ANIMALS;
-                } else {
-                    System.out.println("\nEnter data to search for animals:");
-                }
+                processSearchQuery(input);
                 break;
             default:
                 break;
         }
+    }
+
+    private void processSearchQuery(String input) {
+            searchForAnimal(input);
+            promptMenu();
+    }
+
+    private void enterAnimal(String input) {
+        if (inputCount > 0) {
+            animals.add(input);
+            inputCount--;
+        }
+        if (inputCount <= 0) {
+            inputCount = 0;
+            promptMenu();
+        }
+    }
+
+    private void promptEnterAnimals() {
+        queryState = QueryState.ANIMAL_ENTRY;
+        System.out.println("Enter all animals:");
+    }
+
+    private void processMenuOption(String input) {
+        switch (input) {
+            case "1":
+                promptQuery();
+                break;
+            case "2":
+                printAll();
+                break;
+            case "0":
+                System.out.println("\nBye!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("\nIncorrect option! Try again.");
+                promptMenu();
+                break;
+        }
+    }
+
+    private void printAll() {
+        System.out.println("\n=== List of animals ===");
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+        promptMenu();
+    }
+
+    private void promptMenu() {
+        queryState = QueryState.MENU_OPTIONS;
+        System.out.println("\n=== Menu ===" +
+                "\n1. Find an animal" +
+                "\n2. Print all animals" +
+                "\n0. Exit");
+    }
+
+    private void promptQuery() {
+        queryState = QueryState.DATA_TO_FIND;
+        System.out.println("\nEnter an animal or description to search " +
+                "all suitable animals");
+    }
+
+    private void promptNumAnimals() {
+        queryState = QueryState.NUMBER_OF_ANIMALS;
+        System.out.println("Enter the number of animals:");
+    }
+
+    private int stringToNumAnimals(String input) {
+        return Integer.parseInt(input); // ToDo: exception check
     }
 
     private void searchForAnimal(String input) {
